e858936635b38ff5fbc5625a29ffb0aea081e14e
hannzebb <vladlen.kriukov@gmail.com>
2020-07-08 20:30:40 +0300

    added rate in products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
-$(function(){
+$(function () {
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true
+    });
 
     var mixer = mixitup('.products__inner-box');
 
-  
 });
