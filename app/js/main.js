c737ef7d0747f7018eb33a501567abf1947257da
hannzebb <vladlen.kriukov@gmail.com>
2020-07-08 21:08:51 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,13 @@ $(function () {
         readOnly: true
     });
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
