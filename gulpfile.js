3a512d98675bd019f4340dad1d42c4b254146c7b
hannzebb <vladlen.kriukov@gmail.com>
2020-07-21 17:37:40 +0300

    added page upload

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,8 @@ gulp.task('css', function () {
       'node_modules/slick-carousel/slick/slick.css',
       'node_modules/rateyo/src/jquery.rateyo.css',
       'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
     ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -63,6 +65,7 @@ gulp.task('js', function () {
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/rateyo/src/jquery.rateyo.js',
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
